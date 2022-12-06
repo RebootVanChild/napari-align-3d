742f299abaa989f2f37bbe992e785c86555f76bb
Tiancheng Liu <tianchengliu.com@gmail.com>
2022-12-06 05:05:42 -0600

    removed other widgets

--- src/napari_align_3d/__init__.py
+++ src/napari_align_3d/__init__.py
@@ -4,10 +4,9 @@ except ImportError:
     __version__ = "unknown"
 
 from ._reader import napari_get_reader
-from ._widget import ExampleQWidget, example_magic_widget
+from ._widget import example_magic_widget
 
 __all__ = (
     "napari_get_reader",
-    "ExampleQWidget",
     "example_magic_widget",
 )
--- src/napari_align_3d/_widget.py
+++ src/napari_align_3d/_widget.py
@@ -13,10 +13,9 @@ import numpy as np
 from aicsimageio import AICSImage
 from magicgui import magic_factory
 from napari import Viewer
-from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget
 
 if TYPE_CHECKING:
-    import napari
+    pass
 
 
 # landmarks file has coordinates in microns,
@@ -116,25 +115,6 @@ def GetAffineMatrixFromLandmarks(
     matrix = np.append(x.reshape(3, 4), [[0.0, 0.0, 0.0, 1.0]], axis=0)
     print(matrix)
     return matrix
-
-
-class ExampleQWidget(QWidget):
-    # your QWidget.__init__ can optionally request the napari viewer instance
-    # in one of two ways:
-    # 1. use a parameter called `napari_viewer`, as done here
-    # 2. use a type annotation of 'napari.viewer.Viewer' for any parameter
-    def __init__(self, napari_viewer):
-        super().__init__()
-        self.viewer = napari_viewer
-
-        btn = QPushButton("Click me!")
-        btn.clicked.connect(self._on_click)
-
-        self.setLayout(QHBoxLayout())
-        self.layout().addWidget(btn)
-
-    def _on_click(self):
-        print("napari has", len(self.viewer.layers), "layers")
 
 
 @magic_factory(
@@ -245,10 +225,3 @@ def example_magic_widget(
             visible=True,
         )
     viewer.dims.ndisplay = 3
-
-
-# Uses the `autogenerate: true` flag in the plugin manifest
-# to indicate it should be wrapped as a magicgui to autogenerate
-# a widget.
-def example_function_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
