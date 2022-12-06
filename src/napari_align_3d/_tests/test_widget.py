b1ac6855a463dabc8eee409e3be8b5f62803c1b7
Tiancheng Liu <tianchengliu.com@gmail.com>
2022-12-06 04:54:15 -0600

    skip example test

--- src/napari_align_3d/_tests/test_widget.py
+++ src/napari_align_3d/_tests/test_widget.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from napari_align_3d import ExampleQWidget, example_magic_widget
+from napari_align_3d import ExampleQWidget
 
 
 # make_napari_viewer is a pytest fixture that returns a napari viewer object
@@ -22,15 +22,16 @@ def test_example_q_widget(make_napari_viewer, capsys):
 
 
 def test_example_magic_widget(make_napari_viewer, capsys):
-    viewer = make_napari_viewer()
-    layer = viewer.add_image(np.random.random((100, 100)))
-
-    # this time, our widget will be a MagicFactory or FunctionGui instance
-    my_widget = example_magic_widget()
-
-    # if we "call" this object, it'll execute our function
-    my_widget(viewer.layers[0])
-
-    # read captured output and check that it's as we expected
-    captured = capsys.readouterr()
-    assert captured.out == f"you have selected {layer}\n"
+    # viewer = make_napari_viewer()
+    # layer = viewer.add_image(np.random.random((100, 100)))
+    #
+    # # this time, our widget will be a MagicFactory or FunctionGui instance
+    # my_widget = example_magic_widget()
+    #
+    # # if we "call" this object, it'll execute our function
+    # my_widget(viewer.layers[0])
+    #
+    # # read captured output and check that it's as we expected
+    # captured = capsys.readouterr()
+    # assert captured.out == f"you have selected {layer}\n"
+    assert True
